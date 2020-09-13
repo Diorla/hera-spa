93c150a401a39be6f3c1fe337bca7a8b8221deaa
Ade Adeola <adedotster@gmail.com>
2020-09-13 16:27:29 +0200

    Add landing to main app

--- src/sections/Landing.js
+++ src/sections/Landing.js
@@ -3,14 +3,14 @@ import React from 'react';
 import "./landing.css";
 import down from "./down.svg";
 
-export function Landing() {
+export default function Landing() {
   return (
     <section className="landing" id="top">
       <nav>
         <h1>Hera Salon</h1>
         <ul>
           <li><a href="#home">Home</a></li>
-          <li><a href="#services">Services</a></li>
+          <li><a href="#service">Service</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#contact">Contact</a></li>
           <li><a href="#staff">Staff</a></li>
