06f649964872c71b47c1da6a83f518116154a013
Ade Adeola <adedotster@gmail.com>
2020-09-13 21:25:20 +0200

    About added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 //@ts-check
-import React from 'react';
-import './App.css';
-import Landing from './sections/Landing';
-import Service from './sections/Service';
+import React from "react";
+import "./App.css";
+import Landing from "./sections/Landing";
+import Service from "./sections/Service";
+import About from "./sections/About";
 
 function App() {
   return (
     <div className="App">
       <Landing />
       <Service />
+      <About />
     </div>
   );
 }
---
+++ src/sections/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./about.css";
+
+function About() {
+  return (
+    <section id="about">
+      <article>
+        <h1>We Specialise in</h1>
+        <div>
+          Do eu laborum dolore adipisicing in non non enim aliquip tempor minim.
+        </div>
+        <div>
+          Do eu laborum dolore adipisicing in non non enim aliquip tempor minim.
+        </div>
+        <div>
+          Do eu laborum dolore adipisicing in non non enim aliquip tempor minim.
+        </div>
+        <div>
+          Do eu laborum dolore adipisicing in non non enim aliquip tempor minim.
+        </div>
+        <div>
+          Do eu laborum dolore adipisicing in non non enim aliquip tempor minim.
+        </div>
+        <div>
+          Do eu laborum dolore adipisicing in non non enim aliquip tempor minim.
+        </div>
+      </article>
+      <article></article>
+    </section>
+  );
+}
+
+export default About;
