a43049f75f186d35649a62d73a4c3079d6ed6e0a
Ade Adeola <adedotster@gmail.com>
2020-09-14 18:08:57 +0200

    SPA doesn't need ./home

--- src/sections/Landing.js
+++ src/sections/Landing.js
@@ -7,9 +7,7 @@ export default function Landing() {
   return (
     <section className="landing" id="top">
       <nav>
-        <h1>Hera Salon</h1>
         <ul>
-          <li><a href="#home">Home</a></li>
           <li><a href="#service">Service</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#gallery">Gallery</a></li>
